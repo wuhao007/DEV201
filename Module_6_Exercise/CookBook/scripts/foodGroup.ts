ea037d9f9a47e6bd99eb35dff39a97d9892dd2ef
Hao Wu <tohaowu@gmail.com>
2015-05-08 08:17:34 -0700

    module 6

--- Module_6_Exercise/CookBook/scripts/foodGroup.ts
+++ Module_6_Exercise/CookBook/scripts/foodGroup.ts
@@ -5,6 +5,7 @@
         //TODO
         //Assign the "name" parameter to the 
         //"name" member variable 
+        this.name = name;
 
 
     }  
--- Module_6_Exercise/CookBook/scripts/initializer.js
+++ Module_6_Exercise/CookBook/scripts/initializer.js
@@ -1,45 +1,36 @@
-﻿var recipeCategories: RecipeCategories;
+var recipeCategories;
 var renderer = null;
-
-window.onload = () => {
-    var categoriesSelect = (<HTMLSelectElement> document.getElementById('RecipeCategory'));
-
+window.onload = function () {
+    var categoriesSelect = document.getElementById('RecipeCategory');
     //TODO
     //categoriesSelect.onchange currently doesn't do anything.
     //Assign a lambda expression to "onchange" that when called, 
     //invokes the "loadRecipes" function that you'll see below. 
     //The lambda parentheses will be empty.
     //HINT: Refer to Module 4:Functions if you need help writing the lambda.
-
-    categoriesSelect.onchange = () => {loadRecipes();};
-
+    categoriesSelect.onchange = function () { loadRecipes(); };
     //TODO
     //Create a new RecipeLoader instance and name it "loader".
     //Pass the following string into the RecipeLoader's constructor:
     //  '/JSON/recipeTypes.json'
     //HINT: Use the "new" keyword to create the instance.
     var loader = new RecipeLoader('/JSON/recipeTypes.json');
-
     //TODO 
     //Call the loader object's load() function ("loader" is the object 
     //you created in the previous TODO)
     loader.load();
-
     renderer = new Renderer();
 };
-
 function loadRecipes() {
-    var el = (<HTMLSelectElement> document.getElementById('RecipeCategory'));
+    var el = document.getElementById('RecipeCategory');
     try {
         var category = recipeCategories.items
-            .filter(i => i.name === el.value)
-            .reduce(i => (new RecipeCategory(el.value, i.foodGroups, i.description)));
+            .filter(function (i) { return i.name === el.value; })
+            .reduce(function (i) { return (new RecipeCategory(el.value, i.foodGroups, i.description)); });
         renderer.renderCategory(category);
     }
-    catch (ex) { alert(ex.message) }
+    catch (ex) {
+        alert(ex.message);
+    }
 }
-  
-
-
-
-
+//# sourceMappingURL=initializer.js.map
