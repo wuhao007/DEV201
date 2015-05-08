128c57dcd30ce9e6f95a9492982b07013c0c151a
Hao Wu <tohaowu@gmail.com>
2015-05-08 07:48:39 -0700

    a change I cannot control

---
+++ Module_6_Exercise/CookBook/scripts/baseRecipeCategory.ts
@@ -0,0 +1,11 @@
+﻿class BaseRecipeCategory {
+    name: string;
+    foodGroups: FoodGroup[] = [];
+
+    //TODO:
+    //Add constructor using the variable names above
+    //as the parameters (so you'll have two parameters). 
+    //Assign the parameter value to the associated member variables.
+
+
+} 
---
+++ Module_6_Exercise/CookBook/scripts/recipeCategory.ts
@@ -0,0 +1,22 @@
+﻿//TODO
+//Make RecipeCategory inherit from BaseRecipeCategory
+//HINT: Use the "extends" keyword
+class RecipeCategory {
+
+    //TODO
+    //Add a constructor that takes the following parameters:
+    //1. name: string
+    //2. foodGroups: FoodGroup[]
+    //3. description: string 
+
+    //Make the description parameter public so that TypeScript
+    //automatically generates a member variable. 
+    //Hint: Use the "public" keyword.
+
+    //Within the constructor, forward the "name" and "foodGroups"
+    //parameter values to the base class
+    //HINT: Use the "super" keyword
+
+
+
+} 
