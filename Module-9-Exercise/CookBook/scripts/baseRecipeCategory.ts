6af11fff6a95eab62f1678bda06467fb7af31540
Hao Wu <tohaowu@gmail.com>
2015-05-11 09:25:53 -0700

    module 9

---
+++ Module-9-Exercise/CookBook/scripts/baseRecipeCategory.ts
@@ -0,0 +1,6 @@
+﻿//FROM MODULES 6-8: 
+//Copy your BaseRecipeCategory class code
+
+class BaseRecipeCategory  {
+  
+} 
---
+++ Module-9-Exercise/CookBook/scripts/recipeCategory.ts
@@ -0,0 +1,7 @@
+﻿//FROM MODULE 7&8:
+//Fill out the body of this class so it extends BaseRecipeCategory
+//and implements IRecipeCategory
+
+class RecipeCategory  {
+    
+} 
